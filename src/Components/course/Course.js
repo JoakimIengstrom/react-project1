9a031dab66b089638ccfb90176d62b508a083bde
Haqjahirul123 <haqjahirul1983@gmail.com>
2022-09-16 14:29:26 +0200

    cart

--- src/Components/course/Course.js
+++ src/Components/course/Course.js
@@ -3,25 +3,28 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 //import Button from 'react-bootstrap/Button';
-import {Button,Card,Container } from "react-bootstrap";
+import {Button,Card,Container,Stack } from "react-bootstrap";
 
 const Course = (props) => {
     const {name, price, instructor, image} = props.course
     return (
 
-        <Container>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={image} />
-                    <Card.Body>
-                        <Card.Title>Couse Title: <span className="course-name">{name}</span></Card.Title>
-                            <Card.Text>
-                                <p>Instructor: <b>{instructor}</b></p>
-                                <p> Price: {price} SEK</p>
-                               
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-            </Card>
+        <Container fluid>
+              <Stack direction="horizontal" gap={5}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={image} />
+                        <Card.Body>
+                            <Card.Title>Couse Title: <span className="course-name">{name}</span></Card.Title>
+                                <Card.Text>
+                                    <p>Instructor: <b>{instructor}</b></p>
+                                    <p> Price: {price} SEK</p>
+                                
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                </Card>
+                </Stack>
+            
         </Container>
     );
 };
