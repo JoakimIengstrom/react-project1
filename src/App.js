a5776b00a98bc8d56c87630da507615bfca48cb5
Haqjahirul123 <haqjahirul1983@gmail.com>
2022-09-16 10:30:35 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import React from "react";
+//import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header/Header";
+
 
 function App() {
-  return <div className="App">React Project</div>;
+ 
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">3J Coder</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Jahirul</Nav.Link>
+          <Nav.Link href="#pricing">Johokim</Nav.Link>
+          <Nav.Link href="#pricing">Jeanette</Nav.Link>
+          
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      <br />
+    </div>
+  );
+};
+
+export default Header;
